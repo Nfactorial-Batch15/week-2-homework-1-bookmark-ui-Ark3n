695b5697bac728d76020f49b79afcb55b4533fa5
Arken <Arken.Sarsenov@outlook.com>
2021-12-08 00:16:56 +0500

    Initial Commit

---
+++ Bookmark UI/Bookmark_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bookmark_UIApp.swift
+//  Bookmark UI
+//
+//  Created by Arken Sarsenov on 08.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Bookmark_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
